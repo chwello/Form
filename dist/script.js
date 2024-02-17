222e6f10ecd543fc3e15bae56684e284011cce5b
chelo.chel <122210462+chwello@users.noreply.github.com>
2024-02-17 16:51:19 +0800

    Add files via upload

---
+++ dist/script.js
@@ -0,0 +1,8 @@
+const firstName = document.getElementById("fname");
+const lastName = document.getElementById("lname");
+const birthDate = document.getElementById("birthdate");
+const email = document.getElementById("email");
+const confirmEmail = document.getElementById("confirm_email");
+const password = document.getElementById("password");
+const confirmPassword = document.getElementById("confirm_password");
+
